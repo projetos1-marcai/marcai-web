fdf7c893f1c51f84538fa7e7f025c692596b4dfe
Marcelo Andrade <MagnerSA>
2022-08-16 18:47:13 -0300

    Adicionadas reservas na página de Perfil do Fornecedor

--- src/app/core/services/user/user.service.ts
+++ src/app/core/services/user/user.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { EnvironmentService } from '../environment/environment.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  api: string = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient, private environmentService: EnvironmentService) { }
+
+  getUser(id: any): Observable<any> {
+    return this.http.get(`${this.environmentService.apiUrl}/user/${id}`);
+  }
 }
